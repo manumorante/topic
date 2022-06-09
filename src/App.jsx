841d08ce38c6a3f225b1f23fec3900c555917212
Manu Morante <manu@estadologico.com>
2022-06-09 12:58:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='App mx-auto max-w-2xl'>
+      <h1 className='text-lg text-black'>
+        Vite + React + Tailwindcss Template
+      </h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A aliquid
+        eligendi libero nostrum neque ipsum, vitae quo necessitatibus mollitia.
+        Dignissimos ex illo ut dolorem doloremque itaque veritatis? Nemo,
+        facilis fuga?
+      </p>
+    </div>
+  )
+}
