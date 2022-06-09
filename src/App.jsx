f55921b8679627415e1c17bfcf12c7fc29343e11
Manu Morante <manu@estadologico.com>
2022-06-09 12:52:32 +0000

    Input your text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { TEXT } from '../text'
 
 export default function App() {
   const [data, setData] = useState(null)
   const [normalized, setNormalized] = useState(null)
+  const textRef = useRef(null)
 
   // Procesa el texto aplicando los siguiente acciones:
   const normalizeText = (text) => {
@@ -48,15 +49,17 @@ export default function App() {
     return dataObject
   }
 
-  useEffect(() => {
-    const pNormalized = normalizeText(TEXT)
-    setNormalized(pNormalized)
+  // Llama a una funcion cuando el textarea cambie de valor
+  const handleRun = () => {
+    const text = textRef.current.value
+    const normalizedText = normalizeText(text)
+    setNormalized(normalizedText)
 
-    const estructured = makeObject(pNormalized)
-    // Ordena el objeto por el número de repeticiones
+    const estructured = makeObject(normalizedText)
     const sorted = Object.entries(estructured).sort((a, b) => b[1] - a[1])
+
     setData(sorted)
-  }, [])
+  }
 
   return (
     <div className='App mx-auto max-w-2xl flex flex-col gap-5'>
@@ -64,7 +67,14 @@ export default function App() {
         <summary className='select-none text-sm font-semibold text-white'>
           Texto bruto
         </summary>
-        <div className='mt-3 text-sm leading-6 text-slate-400'>{TEXT}</div>
+        <div className='mt-3 text-sm leading-6 text-slate-400'>
+          <textarea
+            ref={textRef}
+            className='bg-slate-900 w-full'
+            defaultValue={TEXT}
+            rows={10}
+          />
+        </div>
       </details>
 
       <details className='open rounded-lg bg-slate-900 p-6 shadow-lg ring-1 ring-white/10'>
@@ -75,6 +85,12 @@ export default function App() {
           {normalized}
         </div>
       </details>
+
+      <button
+        className='bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400'
+        onClick={handleRun}>
+        Run
+      </button>
 
       {/* Pinta la data en forma de tabla */}
       {data && (
